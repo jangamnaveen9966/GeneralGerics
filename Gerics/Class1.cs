b00b52a02c05803cb6a9d4e96ac11ebf85ed9ee4
jn996 <jn996@JANGAM>
2024-07-04 00:50:28 +0530

    Add project files.

---
+++ Gerics/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gerics
+{
+    public class Class1
+    {
+       public static void  Main(string[] args)
+        {
+            dispaly<int>(1, 2);
+            dispaly<string>("Test", "jsdfj");
+
+        }
+         public static T dispaly<T>(T v1 ,T v2)
+        {
+              dynamic result = v1;
+               result += (dynamic)v2;
+               Console.WriteLine(result);
+               return result;
+        }
+    }
+}
